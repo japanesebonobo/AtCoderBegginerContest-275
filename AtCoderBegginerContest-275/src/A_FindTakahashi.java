be355c6451377e287a9333eff74f3f1e73d9f499
japanesebonobo <tomi119119@gmail.com>
2022-10-30 18:59:01 +0900

    refactorA&B

--- AtCoderBegginerContest-275/src/A_FindTakahashi.java
+++ AtCoderBegginerContest-275/src/A_FindTakahashi.java
@@ -1,31 +1,22 @@
 package src;
- import java.util.*;
+import java.util.*;
 
- public class A_FindTakahashi {
-     public static void main(String[] args) {
-         Scanner scanner = new Scanner(System.in);
-         int N = scanner.nextInt();
-         List<List<Integer>> list = new ArrayList<>(N);
-         for (int i = 0; i < N; i++) {
-             List<Integer> partList = new ArrayList<>(N);
-             int n = scanner.nextInt();
-             partList.add(i);
-             partList.add(n);
-             list.add(partList);
-         }
-         scanner.close();
+public class A_FindTakahashi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int H[] = new int[N];
+        int max = 0;
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            H[i] = scanner.nextInt();
+            if (max < H[i]) {
+            max = H[i];
+            ans = i+1;
+            }
+        }
+        scanner.close();
 
-         List<Integer> max = new ArrayList<>();
-         max.add(0);
-         max.add(0);
-
-         for (int i = 0; i < list.size(); i++) {
-             if(max.get(1) <= list.get(i).get(1)) {
-                 max.add(0,list.get(i).get(0));
-                 max.add(1,list.get(i).get(1));
-             }
-         }
-
-         System.out.println(max.get(0)+1);
-     }
- }
+        System.out.println(ans);
+    }
+}
--- AtCoderBegginerContest-275/src/B_LineSensor.java
+++ AtCoderBegginerContest-275/src/B_LineSensor.java
@@ -1,40 +1,26 @@
 
- package src;
- import java.math.BigInteger;
- import java.util.*;
+package src;
+import java.math.BigInteger;
+import java.util.*;
 
- public class B_LineSensor {
-     public static void main(String[] args) {
-         Scanner scanner = new Scanner(System.in);
+public class B_LineSensor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-         long A = scanner.nextLong();
-         long B = scanner.nextLong();
-         long C = scanner.nextLong();
-         long D = scanner.nextLong();
-         long E = scanner.nextLong();
-         long F = scanner.nextLong();
+        int divisor = 998244353;
 
-         String a = String.valueOf(A);
-         String b = String.valueOf(B);
-         String c = String.valueOf(C);
-         String d = String.valueOf(D);
-         String e = String.valueOf(E);
-         String f = String.valueOf(F);
+        long input[] = new long[6];
+        for (int i = 0; i < 6; i++) {
+            input[i] = scanner.nextLong();
+            input[i] %= divisor;
+        }
+        scanner.close();
 
-         BigInteger aBig = new BigInteger(a);
-         BigInteger bBig = new BigInteger(b);
-         BigInteger cBig = new BigInteger(c);
-         BigInteger dBig = new BigInteger(d);
-         BigInteger eBig = new BigInteger(e);
-         BigInteger fBig = new BigInteger(f);
+        long ABC = (input[0]*input[1]%divisor)*input[2]%divisor;
+        long DEF = (input[3]*input[4]%divisor)*input[5]%divisor;
 
-         BigInteger one = aBig.multiply(bBig).multiply(cBig);
-         BigInteger two = dBig.multiply(eBig).multiply(fBig);
+        long ans = (ABC-DEF+divisor)%divisor;
 
-         BigInteger ansBig = one.subtract(two);
-         BigInteger num = new BigInteger("998244353");
-         String ans = ansBig.remainder(num).toString();
-
-         System.out.println(ans);
-     }
- }
+        System.out.println(ans);
+    }
+}
